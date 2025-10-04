97b5618413738ef4026a431d1a5d80072c78dbdb
neelesh yadav <neelesh6kumar@gmail.com>
2025-10-04 20:54:48 +0530

    deployment commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import Coin from './pages/Coin/Coin';
-import { CoinContextProvider } from './context/CoinContext';
+import CoinContextProvider from './context/CoinContext';
 import './index.css'; // Changed from './Index.css' to './index.css'
 
 const App = () => {
