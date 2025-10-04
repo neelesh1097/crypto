535da0d60610c2a7bdc8a3fae4488a46173fb023
neelesh yadav <neelesh6kumar@gmail.com>
2025-10-04 20:46:06 +0530

    bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import Home from './pages/Home/Home'
-import Coin from './pages/Coin/Coin'
-import CoinContextProvider from './context/CoinContext.jsx'
-import './Index.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Coin from './pages/Coin/Coin';
+import { CoinContextProvider } from './context/CoinContext';
+import './index.css'; // Changed from './Index.css' to './index.css'
 
 const App = () => {
   return (
@@ -26,4 +26,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
